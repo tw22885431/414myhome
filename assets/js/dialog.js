044a6bddef3a29a6b17d345b3c9edf64ed33ad95
ben <re22885431@gmail.com>
2017-08-05 16:27:39 +0800

    modify the login and signup layout

---
+++ assets/js/dialog.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$("#usrlogin").click(function(){
+		$("#usrlogin").modal();
+	});
+});
+$(document).ready(function(){
+	$("#register").click(function(){
+		$("#register").modal();
+	});
+});
